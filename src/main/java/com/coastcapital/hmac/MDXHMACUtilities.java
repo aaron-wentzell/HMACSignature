7fa27745ac314c29a40c6f3bc8d9f703bae84d36
Graham Fraser <gfraser90@gmail.com>
2017-12-06 12:30:14 -0700

    added compare method, fixed utilities, removed hex method, added JUnit tests

--- src/main/java/com/coastcapital/hmac/MDXHMACUtilities.java
+++ src/main/java/com/coastcapital/hmac/MDXHMACUtilities.java
@@ -3,16 +3,13 @@ package com.coastcapital.hmac;
 import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.codec.binary.Hex;
 import org.apache.commons.codec.digest.DigestUtils;
-import org.apache.commons.codec.digest.HmacAlgorithms;
-import org.apache.commons.codec.digest.HmacUtils;
+
 
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
-import java.security.Signature;
-import java.security.SignatureException;
-import java.util.Formatter;
+
 
 //import org.apache.logging.log4j.Logger;
 //import org.apache.logging.log4j.LogManager;
@@ -25,7 +22,9 @@ public class MDXHMACUtilities {
     private static final String HMAC_SHA1_ALGORITHM = "HmacSHA1";
 
     /**
-     * This method is used to create an MD5Hash out of a method payload
+     *
+     * @param payload the inbound http body
+     * @return MD5 hash of the inbound http body
      */
     public static String createMD5Hash (String payload) {
         String digest = DigestUtils.md5Hex(payload);
@@ -33,10 +32,21 @@ public class MDXHMACUtilities {
     }
 
     /**
-     * This method is used to create the HMAC Signature
+     *
+     * @param httpVerb The http method being used
+     * @param md5Hash The MD5 hash of the payload
+     * @param contentTypeHeader http content type
+     * @param unixEpochTime date converted to unix epoch
+     * @param acceptHeader http accept header
+     * @param mdxSessionKey session key if required for MX Endpoint
+     * @param resouceRequest http resource requested
+     * @param hmacSalt
+     * @return Hash built to spec for MX
+     * @throws NoSuchAlgorithmException
+     * @throws InvalidKeyException
      */
     public static String createHMACSignature(String httpVerb, String md5Hash, String contentTypeHeader, String unixEpochTime, String acceptHeader, String mdxSessionKey, String resouceRequest, String hmacSalt)
-        throws NoSuchAlgorithmException, InvalidKeyException, SignatureException{
+            throws NoSuchAlgorithmException, InvalidKeyException{
 
         String combinedString = httpVerb.concat("\n")
                 .concat(md5Hash)
@@ -50,32 +60,57 @@ public class MDXHMACUtilities {
                 .concat(mdxSessionKey)
                 .concat("\n").concat(resouceRequest);
 
-        combinedString = "POST\ne9a179f879165fd64bdeaa57032d342f\napplication/vnd.moneydesktop.mdx.v5+xml\n1382975431\napplication/vnd.moneydesktop.mdx.v5+xml\n\n/sessions";
+        //Test string and console logs
+        //combinedString = "POST\ne9a179f879165fd64bdeaa57032d342f\napplication/vnd.moneydesktop.mdx.v5+xml\n1382975431\napplication/vnd.moneydesktop.mdx.v5+xml\n\n/sessions";
+        //System.out.println(combinedString);
+        //System.out.println(hmacSalt);
 
-        System.out.println(combinedString);
-        System.out.println(hmacSalt);
-
-
-        SecretKeySpec signingKey = new SecretKeySpec(hmacSalt.getBytes(), HMAC_SHA1_ALGORITHM);
+        byte[] decoded = Base64.decodeBase64(hmacSalt);
+        SecretKeySpec signingKey = new SecretKeySpec(decoded, HMAC_SHA1_ALGORITHM);
         Mac mac = Mac.getInstance(HMAC_SHA1_ALGORITHM);
         mac.init(signingKey);
         byte[] finalBytes = mac.doFinal(combinedString.getBytes());
-
-
         String returnStr = Hex.encodeHexString(finalBytes);
         return returnStr;
     }
 
+    /**
+     *
+     * @param inboundMD5Hash inbound hash from the inbound request
+     * @param inboundHMACHash inbound hash from the inbound request
+     * @param payload inbound payload
+     * @param httpVerb inbound http method
+     * @param contentTypeHeader inbound content type header
+     * @param unixEpochTime inbound date converted to unix epoch time
+     * @param acceptHeader inbound http accept header
+     * @param mdxSessionKey inbound session key if required
+     * @param resouceRequest inbound http resource request
+     * @param hmacSalt
+     * @return boolean indicator of message integrity
+     * @throws InvalidKeyException
+     * @throws NoSuchAlgorithmException
+     */
+    public static boolean compareHashes(String inboundMD5Hash, String inboundHMACHash, String payload,String httpVerb, String contentTypeHeader, String unixEpochTime, String acceptHeader, String mdxSessionKey, String resouceRequest, String hmacSalt) throws InvalidKeyException, NoSuchAlgorithmException {
 
-    private static String toHexString(byte[] bytes) {
-        Formatter formatter = new Formatter();
+        boolean bool;
 
-        for (byte b : bytes) {
-            formatter.format("%02x", b);
+        String md5Hash = createMD5Hash(payload);
+        String hmacHash = createHMACSignature(httpVerb,md5Hash,contentTypeHeader,unixEpochTime,acceptHeader,mdxSessionKey,resouceRequest,hmacSalt);
+
+        if (md5Hash.equals(inboundMD5Hash)){
+
+            if(hmacHash.equals(inboundHMACHash)){
+                bool = true;
+                return bool;
+            } else {
+                bool = false;
+                return bool;
+            }
+
+        } else {
+            bool = false;
+            return bool;
         }
-
-        return formatter.toString();
     }
-
 
 }
--- src/test/java/com/coastcapital/jmac/MDXHMACUtilitiesTest.java
+++ src/test/java/com/coastcapital/jmac/MDXHMACUtilitiesTest.java
@@ -1,11 +1,13 @@
 package com.coastcapital.jmac;
 
 import com.coastcapital.hmac.MDXHMACUtilities;
-import com.coastcapital.hmac.HMACHash;
 import org.junit.Test;
 
 
-import static org.junit.Assert.assertEquals;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+import static org.junit.Assert.*;
 
 /**
  * Created by aaronwentzell on 2017-12-04.
@@ -13,7 +15,7 @@ import static org.junit.Assert.assertEquals;
 public class MDXHMACUtilitiesTest {
 
     @Test
-    public void testMD5() {
+    public void positiveTestMD5() {
        String payload =
                "<?xml version=\"1.0\"?>\n<mdx version=\"5.0\">\n  <session>\n    <userkey><![CDATA[the-userkey]]></userkey>\n  </session>\n</mdx>\n";
 
@@ -22,8 +24,8 @@ public class MDXHMACUtilitiesTest {
         assertEquals("Payload MD5 not returning as expected","e9a179f879165fd64bdeaa57032d342f",md5Hash);
     }
 
-/**   @Test
-    public void testHMACSignature() {
+  @Test
+    public void positveTestHMACSignature() {
         String httpVerb = "POST";
         String md5Hash = "e9a179f879165fd64bdeaa57032d342f";
         String contentTypeHeader = "application/vnd.moneydesktop.mdx.v5+xml";
@@ -39,22 +41,102 @@ public class MDXHMACUtilitiesTest {
         } catch(Exception ex){
             assertEquals("Exception Occured in HMAC Signature",1,2);
         }
-
+        assertNotNull("HMAC Signature is null",hmacSignature);
         assertEquals("HMAC Signature Not Equal","e47928dcd29e494116961ad12884c8fd7aae07f2",hmacSignature);
     }
-**/
-    @Test
-    public void testHMACHash() {
 
-        String hmacHash = "";
-        String secretKey = "QUJDREVGR0hJSktMTU5PUFFSU1RVVldYWVo3ODkwMTI=";
-        String message = "POST\ne9a179f879165fd64bdeaa57032d342f\napplication/vnd.moneydesktop.mdx.v5+xml\n1382975431\napplication/vnd.moneydesktop.mdx.v5+xml\n\n/sessions";
+    @Test
+    public void negativeTestMD5() {
+        String payload =
+                "<?xml version=\"1.0\"?>\n<mdx version=\"5.0\">\n  <session>\n    <userkey><![CDATA[the-userkey]]>I will break the test</userkey>\n  </session>\n</mdx>\n";
+
+        String md5Hash = MDXHMACUtilities.createMD5Hash(payload);
+        assertNotNull("Payload MD5 is null",md5Hash);
+        assertNotEquals("Payload MD5 should not match","e9a179f879165fd64bdeaa57032d342f",md5Hash);
+    }
+
+    @Test
+    public void negativeTestHMACSignature() {
+        String httpVerb = "POST";
+        String md5Hash = "e9a179f879165fd64bdeaa57032d342f";
+        String contentTypeHeader = "application/vnd.moneydesktop.mdx.v5+xml";
+        String unixEpochTime = "1382975431";
+        String acceptHeader="application/vnd.moneydesktop.mdx.v5+xml";
+        String mdxSessionKey="I Will break the test case";
+        String resouceRequest="/sessions";
+        String hmacSalt = "QUJDREVGR0hJSktMTU5PUFFSU1RVVldYWVo3ODkwMTI=";
+
+        String hmacSignature = "";
         try{
-            hmacHash=HMACHash.generateHash(secretKey,message);
+            hmacSignature=MDXHMACUtilities.createHMACSignature(httpVerb,md5Hash,contentTypeHeader,unixEpochTime,acceptHeader,mdxSessionKey,resouceRequest,hmacSalt);
         } catch(Exception ex){
-            assertEquals("Exception Occured in generateHash",1,2);
+            assertEquals("Exception Occured in HMAC Signature",1,2);
+        }
+        assertNotNull("HMAC Signature is null",hmacSignature);
+        assertNotEquals("HMAC Signature should not match","e47928dcd29e494116961ad12884c8fd7aae07f2",hmacSignature);
+    }
+
+    @Test
+    public void combinedTest() {
+
+        //Grab MD5 Hash
+        String payload =
+                "<?xml version=\"1.0\"?>\n<mdx version=\"5.0\">\n  <session>\n    <userkey><![CDATA[the-userkey]]></userkey>\n  </session>\n</mdx>\n";
+        String md5Hash = MDXHMACUtilities.createMD5Hash(payload);
+        assertEquals("Payload MD5 not returning as expected","e9a179f879165fd64bdeaa57032d342f",md5Hash);
+
+        //Grab HMAC Hash
+        String httpVerb = "POST";
+        String contentTypeHeader = "application/vnd.moneydesktop.mdx.v5+xml";
+        String unixEpochTime = "1382975431";
+        String acceptHeader="application/vnd.moneydesktop.mdx.v5+xml";
+        String mdxSessionKey="";
+        String resouceRequest="/sessions";
+        String hmacSalt = "QUJDREVGR0hJSktMTU5PUFFSU1RVVldYWVo3ODkwMTI=";
+        String hmacSignature = "";
+        try{
+            hmacSignature=MDXHMACUtilities.createHMACSignature(httpVerb,md5Hash,contentTypeHeader,unixEpochTime,acceptHeader,mdxSessionKey,resouceRequest,hmacSalt);
+        } catch(Exception ex){
+            assertEquals("Exception Occured in HMAC Signature",1,2);
+        }
+        assertNotNull("HMAC Signature is null",hmacSignature);
+        assertEquals("HMAC Signature Not Equal","e47928dcd29e494116961ad12884c8fd7aae07f2",hmacSignature);
+    }
+
+    @Test
+    public void positiveCompareTest(){
+        //Grab MD5 Hash
+        String payload =
+                "<?xml version=\"1.0\"?>\n<mdx version=\"5.0\">\n  <session>\n    <userkey><![CDATA[the-userkey]]></userkey>\n  </session>\n</mdx>\n";
+        String md5Hash = MDXHMACUtilities.createMD5Hash(payload);
+
+        //Grab HMAC Hash
+        String httpVerb = "POST";
+        String contentTypeHeader = "application/vnd.moneydesktop.mdx.v5+xml";
+        String unixEpochTime = "1382975431";
+        String acceptHeader="application/vnd.moneydesktop.mdx.v5+xml";
+        String mdxSessionKey="";
+        String resouceRequest="/sessions";
+        String hmacSalt = "QUJDREVGR0hJSktMTU5PUFFSU1RVVldYWVo3ODkwMTI=";
+        String hmacSignature = "";
+        try{
+            hmacSignature=MDXHMACUtilities.createHMACSignature(httpVerb,md5Hash,contentTypeHeader,unixEpochTime,acceptHeader,mdxSessionKey,resouceRequest,hmacSalt);
+        } catch(Exception ex){
+            assertEquals("Exception Occured in HMAC Signature",1,2);
         }
 
-        assertEquals("HMAC Hash Not Equal","e47928dcd29e494116961ad12884c8fd7aae07f2",hmacHash);
+        //Compare
+        boolean bool = false;
+        try {
+            bool = MDXHMACUtilities.compareHashes(md5Hash,hmacSignature,payload,httpVerb,contentTypeHeader,unixEpochTime,acceptHeader,mdxSessionKey,resouceRequest,hmacSalt);
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        assertTrue("Hashes are not equal",bool);
+
+
     }
+
 }
